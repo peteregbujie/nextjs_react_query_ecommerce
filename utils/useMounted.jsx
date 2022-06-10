a206a1c325fb5d4638ffac54cdd5699cace864dd
Peter Egbujie <ogbillion@gmail.com>
2022-06-10 00:06:32 -0700

    Added UseMount custom hook to prevent hydration issues.

--- utils/useMounted.jsx
+++ utils/useMounted.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 /*useMounted() is a simple hook used to prevent react hydration issues. The hook provides us the information about
- the component being rendered.  That way we can set the actual amount of items in cart after the first render and 
+ the component being rendered.  That way, we can get the actual amount of items in cart after the first render and 
  end up with the server content initially being the same than the client content*/
 
 export const useMounted = () => {
