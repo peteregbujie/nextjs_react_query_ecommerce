3f66335d7dcfa9ec2816bdfaa5f4582572819f00
Peter Egbujie <ogbillion@gmail.com>
2022-06-09 23:52:04 -0700

    Redesigned Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,15 @@
+import { ShoppingBagIcon } from "@heroicons/react/outline";
 import Link from "next/link";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { useMounted } from "../utils/useMounted";
 import { CartContext } from "./cart/context/CartContext";
 
-const links = [
- {
-  href: "/",
-  text: "Home",
- },
-];
+// const links = [
+//  {
+//   href: "/",
+//   text: "Home",
+//  },
+// ];
 
 const Navbar = () => {
  const { hasMounted } = useMounted();
@@ -19,93 +20,70 @@ const Navbar = () => {
  } = state;
 
  const AllCartItems = hasMounted ? cartItems : "";
- console.log(AllCartItems.length);
-
- const [active, setActive] = useState(false);
-
- const handleClick = () => {
-  setActive(!active);
- };
 
  return (
-  <>
-   <nav className="flex flex-wrap items-center p-3 bg-blue-400 ">
+  <div className="bg-white">
+   <div className="flex items-center h-16 max-w-screen-xl gap-8 px-4 mx-auto sm:px-6 lg:px-8">
     <Link href="/">
-     <a className="inline-flex items-center p-2 mr-4 ">
-      <span className="text-xl font-bold tracking-wide text-white uppercase">
-       Shoe Store
-      </span>
+     <a className="block text-teal-600">
+      <span className="sr-only">Home</span>
+      <h1 className="font-bold">SHOE PLAZA</h1>
      </a>
     </Link>
-    <button
-     className="inline-flex p-3 ml-auto text-white rounded outline-none hover:bg-blue-600 lg:hidden hover:text-white"
-     onClick={handleClick}
-    >
-     <svg
-      className="w-6 h-6"
-      fill="none"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-     >
-      <path
-       strokeLinecap="round"
-       strokeLinejoin="round"
-       strokeWidth={2}
-       d="M4 6h16M4 12h16M4 18h16"
-      />
-     </svg>
-    </button>
-    {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-    <div
-     className={`${
-      active ? "" : "hidden"
-     }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
-    >
-     <div>
-      <ul className="flex flex-row w-full list-none justify-evenly lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto lg:items-center lg:h-auto ">
-       {links.map((link) => (
-        <li key={link.href}>
-         <Link href={link.href}>
-          <a className="items-center justify-center w-full px-3 py-2 font-bold text-white rounded lg:inline-flex lg:w-auto hover:bg-blue-600 hover:text-white ">
-           {link.text}
-          </a>
-         </Link>
-        </li>
-       ))}
+
+    <div className="flex items-center justify-end flex-1 md:justify-between">
+     <nav className="hidden md:block" aria-labelledby="header-navigation">
+      <h2 className="sr-only" id="header-navigation">
+       Header navigation
+      </h2>
+
+      <ul className="flex items-center gap-6 text-sm">
        <li>
-        <Link href="/cart">
-         <a>
-          {AllCartItems.length > 0 ? (
-           <div className="flex">
-            <svg
-             color="white"
-             xmlns="http://www.w3.org/2000/svg"
-             className="flex-1 w-6 h-6 mr-0.1"
-             fill="none"
-             viewBox="0 0 24 24"
-             stroke="currentColor"
-             strokeWidth="2"
-            >
-             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-             />
-            </svg>
-            <div className="flex-1 text-white">{AllCartItems.length}</div>
-           </div>
-          ) : (
-           <div></div>
-          )}
+        <Link href="/products">
+         <a className="text-gray-500 transition hover:text-gray-500/75">
+          Products
          </a>
         </Link>
        </li>
       </ul>
+     </nav>
+
+     <div className="flex items-center gap-4">
+      <div className="flow-root ml-4 lg:ml-6">
+       <Link href="/cart">
+        <a href="#" className="flex items-center p-2 -m-2 group">
+         <ShoppingBagIcon
+          className="flex-shrink-0 w-6 h-6 text-gray-700 group-hover:text-gray-800"
+          aria-hidden="true"
+         />
+         <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+          {AllCartItems.length}
+         </span>
+        </a>
+       </Link>
+      </div>
+
+      <button className="block p-2.5 text-gray-600 transition bg-gray-100 rounded md:hidden hover:text-gray-600/75">
+       <span className="sr-only">Toggle menu</span>
+       <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="w-5 h-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
+       >
+        <path
+         strokeLinecap="round"
+         strokeLinejoin="round"
+         d="M4 6h16M4 12h16M4 18h16"
+        />
+       </svg>
+      </button>
      </div>
     </div>
-   </nav>
-  </>
+   </div>
+  </div>
  );
 };
 export default Navbar;
