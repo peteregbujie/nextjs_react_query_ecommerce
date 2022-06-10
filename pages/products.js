3f5ee0305586bcbd9fe446662d8e97c96cca1c80
Peter Egbujie <ogbillion@gmail.com>
2022-06-10 00:03:53 -0700

    Removed products component and added product page

--- pages/products.js
+++ pages/products.js
@@ -13,7 +13,7 @@ export const Products = () => {
       <h2 className="sr-only">Products</h2>
 
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-       {Array.from(products).map((product) => (
+       {products.map((product) => (
         <Link
          href={`/product/${product.slug}`}
          key={product.name}
