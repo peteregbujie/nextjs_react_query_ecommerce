19e3f454ffe38056eef3c57d6862b6221b261aca
Peter Egbujie <ogbillion@gmail.com>
2022-06-13 00:50:31 -0700

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import { dehydrate, QueryClient } from "react-query";
 import Banner from "../components/Banner";
-import { fetchProducts } from "../utils/rqHooks";
 
 export default function Home() {
  return (
@@ -8,18 +6,4 @@ export default function Home() {
    <Banner />
   </>
  );
-}
-
-export async function getServerSideProps() {
- const queryClient = new QueryClient();
-
- // Use the prefetchQuery method to prefetch the results of a query to be placed into the cache
- await queryClient.prefetchQuery("products", fetchProducts);
-
- // Pass data to the page via props
- return {
-  props: {
-   dehydratedState: dehydrate(queryClient),
-  },
- };
 }
